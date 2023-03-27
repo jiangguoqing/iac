80e0a82313d7fb681b368b9662b408fc09e30105
jiangguoqing <mrjiangguoqing@gmail.com>
2023-03-27 11:02:25 +0800

    ok

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+
+  backend "s3" {
+    bucket = "tf-4"
+    key    = "terraform.tfstate"
+    region = "ap-southeast-1"
+    dynamodb_table = "test"
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
+
