1fc0c74253fbd2ca11f4d0804d998d1d8ca8c5cc
jiangguoqing <mrjiangguoqing@gmail.com>
2023-03-29 11:51:44 +0800

    ok

--- main.tf
+++ main.tf
@@ -21,12 +21,4 @@ provider "aws" {
   region  = "us-west-2"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "ExampleApp"
-  }
-}
 
